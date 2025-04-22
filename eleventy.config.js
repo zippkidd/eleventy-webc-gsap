4fbf4627755eaa9da17d4034b3aa659695f31578
zippkidd <thomastylek@gmail.com>
2025-04-22 00:06:42 -0400

    handle scss and sass files

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,34 @@
 import pluginWebc from '@11ty/eleventy-plugin-webc'
+import path from 'node:path'
+import * as sass from 'sass'
 
 export default function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginWebc, {
     components: '_includes/webc/*.webc'
+  })
+  eleventyConfig.addExtension('scss', {
+    outputFileExtension: 'css',
+
+    compile: async function (inputContent, inputPath) {
+      const parsed = path.parse(inputPath)
+      // Don't compile file names that start with an underscore
+      if (parsed.name.startsWith('_')) {
+        return
+      }
+
+      // Run file content through Sass
+      const result = sass.compileString(inputContent, {
+        loadPaths: [parsed.dir || '.'],
+        sourceMap: true
+      })
+
+      // Allow included files from @use or @import to
+      // trigger rebuilds when using --incremental
+      this.addDependencies(inputPath, result.loadedUrls)
+
+      return async () => {
+        return result.css
+      }
+    }
   })
 };
